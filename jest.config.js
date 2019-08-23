762e64664c8e6780fb845c684149dca1821997b7
Chris Oliver <cado82@gmail.com>
2019-08-23 08:25:43 +0100

    Fix test framework

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     preset: 'jest-preset-angular',
     roots: ['src'],
-    setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts']
+    setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+    globals: {
+        'ts-jest': {
+            tsConfig: '<rootDir>/src/tsconfig.spec.json',
+            diagnostics: false
+        },
+    },
 };
-  
