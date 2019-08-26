b17653b31368db27c5fabb281c01311166964556
Chris Oliver <cado82@gmail.com>
2019-08-26 16:26:16 +0100

    Fix environment urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment: any = {
     production: true,
-    api_url: "https://api.moneteer.com/api/"
+    api_url: "https://api.moneteer.com/api/",
+    auth_url: "https://identity.moneteer.com",
+    auth_redirect_url: "https://app.moneteer.com/auth-callback"
 };
